94a2c75469126f08b5f2fcaacb5d5e98007f60b6
krukrue <kvashnin23@gmail.com>
2026-05-30 22:53:52 +0200

    add tailwind, fix eslint bugs

--- app/app.vue
+++ app/app.vue
@@ -2,5 +2,9 @@
   <div>
     <NuxtRouteAnnouncer />
     <NuxtWelcome />
+    <h1 class="bg-black">
+      Privet kak dela
+      <Icon name="tabler:map"/>
+    </h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,19 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint'],
-
+  modules: ["@nuxt/eslint", "@nuxt/icon"],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  css: ["./app/assets/css/main.css"],
   eslint: {
     config: {
-      standalone: false
-    }
-  }
-})
+      standalone: false,
+    },
+  },
+});
